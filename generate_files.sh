23a256567942efcbaea1347313d1018844a9b1eb
tryal-leaf <katie@parkvue.com>
2021-05-17 23:02:24 -0700

    Add version to datapack

--- generate_files.sh
+++ generate_files.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-## Compress data pack
-rm ".\build\phoenix-datapack.zip"
-"C:\Program Files\7-Zip\7z" a ".\build\phoenix-datapack.zip" ".\datapack\*" ".\README.md" ".\LICENSE"
+# Compress data pack
+VERSION="1.0.2"
+rm -rf ./build/libs/phoenix-datapack-$VERSION.zip
+"C:\Program Files\7-Zip\7z" a ".\build\libs\phoenix-datapack-$VERSION.zip" ".\datapack\*" ".\README.md" ".\LICENSE"
 
 # Generate modpack jars
 ./gradlew build
