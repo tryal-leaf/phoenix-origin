c3f71707ce42bf0b56adb138fbd99ca1a4144241
tryal-leaf <katie@parkvue.com>
2022-05-15 08:29:52 -0500

    Version bump to 1.1.0

--- generate_files.sh
+++ generate_files.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Compress data pack
-VERSION="1.0.4"
+VERSION="1.1.0"
 rm -rf ./build/libs/phoenix-datapack-$VERSION.zip
 "C:\Program Files\7-Zip\7z" a ".\build\libs\phoenix-datapack-$VERSION.zip" ".\datapack\*" ".\README.md" ".\LICENSE"
 
